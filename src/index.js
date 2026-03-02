e4999cdd077c9d1b8ad885a774522095efea3a90
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2026-03-02 19:12:25 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ const bankrollRoutes = require('./routes/bankrollRoutes');
 const app = express();
 const sequelize = require('./database');
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
